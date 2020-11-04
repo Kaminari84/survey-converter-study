895ed3ca4e6bdd9b095d1df00b5f1b5e44e5374c
Kaminari84 <rafal.kocielnik@gmail.com>
2020-11-04 15:44:33 -0800

    Testing env vars on remote

---
+++ test_env_db.py
@@ -0,0 +1,9 @@
+import os
+
+DB_USER = os.getenv('DB_USER')
+DB_PASS = os.getenv('DB_PASS')
+DB_NAME = os.getenv('DB_NAME')
+
+print("DB_USER=", DB_USER)
+print("DB_PASS=", DB_PASS)
+print("DB_NAME=", DB_NAME)
