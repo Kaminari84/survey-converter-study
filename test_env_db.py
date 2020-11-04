6467a7c239ba693d3e426c569026a03bcc09a439
Kaminari84 <rafal.kocielnik@gmail.com>
2020-11-04 15:56:37 -0800

    Update to test env file to test

--- test_env_db.py
+++ test_env_db.py
@@ -4,6 +4,7 @@ DB_USER = os.getenv('DB_USER')
 DB_PASS = os.getenv('DB_PASS')
 DB_NAME = os.getenv('DB_NAME')
 
+print("Testing env variables...")
 print("DB_USER=", DB_USER)
 print("DB_PASS=", DB_PASS)
 print("DB_NAME=", DB_NAME)
