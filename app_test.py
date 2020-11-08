36ec323bc48bcb14fd35f138dfa7ca2efb1ec108
Kaminari84 <rafal.kocielnik@gmail.com>
2020-11-08 00:46:34 -0800

    Adding test flask app file

---
+++ app_test.py
@@ -0,0 +1,8 @@
+from flask import Flask, request, make_response, render_template, current_app, g
+
+app = Flask(__name__)
+
+# Main study
+@app.route('/', methods = ['GET','POST'])
+def study_main():
+  return "Hellow world from Test App!"
