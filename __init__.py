71db34a7a5afb09d83f10fad0f8746b4da078513
Kaminari84 <rafal.kocielnik@gmail.com>
2020-11-08 01:28:36 -0800

    Added env variables for DB access

--- __init__.py
+++ __init__.py
@@ -1,8 +1,13 @@
+import os
 from flask import Flask, request, make_response, render_template, current_app, g
+
+DB_USER = os.getenv('DB_USER')
+DB_PASS = os.getenv('DB_PASS')
+DB_NAME = os.getenv('DB_NAME')
 
 app = Flask(__name__)
 
 # Main study
 @app.route('/', methods = ['GET','POST'])
 def study_main():
-  return "Hello world from Test App & Rafsal!"
+  return f"Hello world from Test App & Rafsal: <br />DB_USER={DB_USER} <br />DB_PASS={DB_PASS} <br /> DB_NAME={DB_NAME}"
