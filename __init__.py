b286f1f770263ab2efb3e2ebf9e63464a3a2c629
Kaminari84 <rafal.kocielnik@gmail.com>
2020-11-08 16:56:54 -0800

    Added AlchemyDB config

--- __init__.py
+++ __init__.py
@@ -1,9 +1,13 @@
 import os
 from flask import Flask, request, make_response, render_template, current_app, g
+from flask_sqlalchemy import SQLAlchemy
+import sqlalchemy
+from sqlalchemy.orm import relationship
 
 ENV_VARS = {}
 
 app = Flask(__name__)
+db = None
 
 def load_env(filename):
   with open(filename) as myfile:
@@ -13,9 +17,21 @@ def load_env(filename):
  
 # Server instance initialize
 def setup_app(app):  
+  global db
+  
   print("Loading the server, first init global vars...")
   print("Root path:", app.root_path)
   load_env(os.path.join(app.root_path,"variables.env"))
+
+  db_name = ENV_VARS.get('DB_NAME')
+  db_user = ENV_VARS.get('DB_USER')
+  db_pass = ENV_VARS.get('DB_PASS')
+
+  app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://"+str(db_user)+":"+str(db_pass)+"@127.0.0.1:3306/"+str(db_name)
+  app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+  app.config['SQLALCHEMY_POOL_RECYCLE'] = 1
+
+  db = SQLAlchemy(app)
 
 
   print("Start the actual server...")
