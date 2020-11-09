a250c1272685daae92e6a1fb16dab11a5f727c0a
Kaminari84 <rafal.kocielnik@gmail.com>
2020-11-08 16:26:19 -0800

    Adding env variables loading

--- __init__.py
+++ __init__.py
@@ -1,18 +1,33 @@
 import os
 from flask import Flask, request, make_response, render_template, current_app, g
 
-DB_USER = os.getenv('DB_USER')
-DB_PASS = os.getenv('DB_PASS')
-DB_NAME = os.getenv('DB_NAME')
+ENV_VARS = {}
 
 app = Flask(__name__)
+
+def load_env(filename):
+  with open(filename) as myfile:
+    for line in myfile:
+      name, var = line.rstrip('\n').partition("=")[::2]
+      ENV_VARS[name.strip()] = var
+ 
+# Server instance initialize
+def setup_app(app):  
+  print("Loading the server, first init global vars...")
+  load_env("variables.env")
+
+
+  print("Start the actual server...")
+
+setup_app(app)
 
 # Main study
 @app.route('/', methods = ['GET','POST'])
 def study_main():
   webHTML = "Hello world from Test App & Rafsal: <br />"
-  webHTML += "DB_USER="+str(DB_USER)+"<br />"
-  webHTML += "DB_PASS="+str(DB_PASS)+"<br />"
-  webHTML += "DB_NAME="+str(DB_NAME)+"<br />"
+  webHTML += "DB_USER="+str(ENV_VARS.get('DB_USER'))+"<br />"
+  webHTML += "DB_PASS="+str(ENV_VARS.get('DB_PASS'))+"<br />"
+  webHTML += "DB_NAME="+str(ENV_VARS.get('DB_NAME'))+"<br />"
+  webHTML += "TEST_VAR="+str(ENV_VARS.get('TEST_VAR'))+"<br />"
 
   return webHTML 
